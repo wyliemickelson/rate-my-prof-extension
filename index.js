6fd53cc79e055dd0096512753ccbd06fe9e928f2
wyliemickelson <wyliecoyote910@gmail.com>
2023-08-22 14:23:32 -0700

    added way to fetch all professors from a school and save it to a json cache

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-import { gql, GraphQLClient } from 'graphql-request'
-import { QueryProfessorData } from './gql-queries.js';
+import { gql, request, GraphQLClient } from 'graphql-request'
+import { QueryProfessorData, QueryTeacherSearch } from './gql-queries.js';
+import util from 'util'
+import fs from 'fs';
+
 
 const API_URL = "https://www.ratemyprofessors.com/graphql"
 const AUTHORIZATION_TOKEN = "Basic dGVzdDp0ZXN0";
@@ -7,13 +10,43 @@ const AUTHORIZATION_TOKEN = "Basic dGVzdDp0ZXN0";
 const client = new GraphQLClient(API_URL, {
   headers: {
     authorization: AUTHORIZATION_TOKEN
-  },
-  fetch
+  }
 })
 
-const FetchProfessorData = async (id) => {
-  const res = await client.request(QueryProfessorData, { id })
-  console.log(res)
-} 
+// WWU School ID
+const WWUID = 'U2Nob29sLTExODQ='
+const FetchAllProfessors = async (schoolID) => {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      Authorization: AUTHORIZATION_TOKEN,
+    },
+    body: JSON.stringify({
+      query: QueryTeacherSearch,
+      variables: {
+        query: {
+            text: "",
+            schoolID,
+            fallback: true,
+            departmentID: null
+        },
+        schoolID
+    }
+    }),
+  });
+  const data = await response.json()
+  let professorList = data.data.search.teachers.edges
+  professorList = professorList.map(prof => prof.node)
+  console.log('Total professors: ', professorList.length)
+  cacheProfessorList(professorList)
+  // console.log(util.inspect(professorList, false, null, true))
+}
 
-FetchProfessorData('VGVhY2hlci0yMzU=')
+const cacheProfessorList = (professors) => {
+  // remove previously cached data
+  fs.truncate('professors.json', 0, () => console.log('cleared cache'))
+  let jsonList = JSON.stringify(professors)
+  fs.writeFile('professors.json', jsonList, 'utf8', () => console.log('saved professors to cache'))
+}
+
+FetchAllProfessors(WWUID)
