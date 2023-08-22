a133f2aed528c9a0f721494a5072e551e1b8ed88
wyliemickelson <wyliecoyote910@gmail.com>
2023-08-22 11:55:50 -0700

    implemented graphql queries and gql wrapper to scrape data

---
+++ gql-queries.js
@@ -0,0 +1,33 @@
+import { gql } from 'graphql-request'
+
+export const QueryProfessorData = gql`
+query ($id: ID!) {
+    node(id: $id) {
+        ... on Teacher {
+            id
+            department
+            legacyId
+            firstName
+            lastName
+            avgRating
+            numRatings
+            avgDifficulty
+            wouldTakeAgainPercent
+        }
+    }
+}
+`
+
+export const QueryProfessorID = gql`
+query ($query: TeacherSearchQuery!) {
+    newSearch {
+        teachers(query: $query) {
+            edges {
+                node {
+                	id
+                }
+            }
+        }
+    }
+}
+`;
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log(document)
+import { gql, GraphQLClient } from 'graphql-request'
+import { QueryProfessorData } from './gql-queries.js';
+
+const API_URL = "https://www.ratemyprofessors.com/graphql"
+const AUTHORIZATION_TOKEN = "Basic dGVzdDp0ZXN0";
+
+const client = new GraphQLClient(API_URL, {
+  headers: {
+    authorization: AUTHORIZATION_TOKEN
+  },
+  fetch
+})
+
+const FetchProfessorData = async (id) => {
+  const res = await client.request(QueryProfessorData, { id })
+  console.log(res)
+} 
+
+FetchProfessorData('VGVhY2hlci0yMzU=')
