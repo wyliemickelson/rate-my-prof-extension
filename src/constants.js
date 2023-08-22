1db1a0665343eb7ae1502eb3ea770c0e00f9803f
wyliemickelson <wyliecoyote910@gmail.com>
2023-08-22 15:28:58 -0700

    moved application start to separate file

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,3 @@
-
 export const API_URL = "https://www.ratemyprofessors.com/graphql"
 export const AUTHORIZATION_TOKEN = "Basic dGVzdDp0ZXN0"
 export const WWUID = 'U2Nob29sLTExODQ=' // WWU School ID
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { FetchAllProfessors } from "./fetching.js"
+import { WWUID } from "./constants.js"
+
+FetchAllProfessors(WWUID)
